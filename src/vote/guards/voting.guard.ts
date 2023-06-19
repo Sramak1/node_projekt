3770c0aec67fb590a86bb540db752146fe113730
Sramak1 <mark.centrih2@scv.si>
2023-06-19 13:07:27 +0100

    change login and fixed voting

---
+++ src/vote/guards/voting.guard.ts
@@ -0,0 +1,18 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class VotingGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const { user } = context.switchToHttp().getRequest();
+    for (const iterator of user.tasks) {
+      console.log(iterator);
+      if (iterator.user.id == user.id) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
